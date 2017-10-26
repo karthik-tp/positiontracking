690a6786c03c0d9576b63b777c245e63c7ec008d
Karthikeyan Pachiappan <kpacha00@gmail.com>
2017-10-26 19:18:59 +1100

    testing jenkins automatic build

--- src/test/java/com/virtualpairprogrammers/TestTimestampParsing.java
+++ src/test/java/com/virtualpairprogrammers/TestTimestampParsing.java
@@ -16,7 +16,7 @@ public class TestTimestampParsing
 	@Test(expected=java.lang.RuntimeException.class)
 	public void invalidTimestampsFailCorrectly() 
 	{
-		// Format: EEE MMM dd HH:mm:ss zzz yyyy		
+		// Format: EEE MMM dd HH:mm:ss zzz yyyy   	
 		Position position = new Position("23", "21", "Monday January 75 aa:15:00 +0000 2017");
 	}
 	
